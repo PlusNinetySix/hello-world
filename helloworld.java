0e6a23a112a66c8fed25040a8c0a0e09551e4dd0
JGPNS <PlusNinetySix@users.noreply.github.com>
2017-08-15 14:06:55 -0500

    Add files via upload

---
+++ helloworld.java
@@ -0,0 +1,23 @@
+package hello;
+public class helloworld {
+	public static void main(String[] args) {
+		
+		int variableName = 5; //first variable
+			char character = 'c'; //character var goes in single quotes
+			String hello = "Hello, World! "; //string var goes in double quotes
+			
+			int x=6; //careful of integer division
+			int y=10; //use double to typecast and get correct answer
+			
+			System.out.println(x!=y); //"==" to check if equal, "!=" to check if not equal
+			System.out.println(x>y); //>= greater than or equal to, <= less than or equal to 
+			//System.out.println(x!=y || x>y);
+			//System.out.println((double)y/x);
+			//System.out.println(hello + variableName + character);
+		//do //bottom tested loop (infinite)
+		 //{
+		 //System.out.println(x);
+		 //x=x+1;
+		 // while(true);
+	}
+}
